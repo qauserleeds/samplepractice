d5284ced041d834ec59bcdcb1eacddc4a41e28fc
qauserleeds <nusumuvenkat@gmail.com>
2016-05-28 18:12:19 +0100

    Program for displaying date

---
+++ CucumberProject/src/test/java/DisplayDate.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class DisplayDate {
+
+	public static void main(String[] args) {
+		Date d = new Date();
+		System.out.println("Date displayed  :" + d.toString());
+
+	}
+
+}
--- CucumberProject/src/test/java/RunTests.java
+++ CucumberProject/src/test/java/RunTests.java
@@ -5,6 +5,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 
+//Runner file
 @Cucumber.Options(format = {"pretty", "html:build/cucumber-html-report"}, tags="@check")
 public class RunTests {
 
